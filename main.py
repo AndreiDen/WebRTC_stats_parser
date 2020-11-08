61ff028c157a8333f5ce9791eb0e04f4b3b4890b
Workstation <andre.simfi@gmail.com>
2020-11-08 15:21:31 +0300

    with unfinished loop through functions

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import json
 from pprint import pprint
 from stats_list import *
 from parse_functions import *
-import time
 from datetime import datetime, timedelta
-from collections import OrderedDict
 
 
 def get_timestamp_list(inbound_audio_packets_received_per_second_stats):
@@ -21,19 +19,17 @@ def get_timestamp_list(inbound_audio_packets_received_per_second_stats):
     return timestamp_list
 
 
-def parse_numeric_stats(stats, stats_title, timestamp_list):
-    for _ in stats.values():
+def add_stats_to_list(stats_list, new_stat, new_stat_title):
+    for _ in new_stat.values():
         v_list = _['values'][1:-1]
         v_list = [round(float(_)) for _ in v_list.split(',')]
-    for _ in timestamp_list:
-        position = timestamp_list.index(_)
-        timestamped_data = timestamp_list[position]
+    for _ in stats_list:
+        position = stats_list.index(_)
+        timestamped_data = stats_list[position]
         timestamped_key = next(iter(timestamped_data))
         timestamped_value = timestamped_data[timestamped_key]
-        timestamped_value[stats_title] = v_list[position]
-
-    return(timestamp_list)
-
+        timestamped_value[new_stat_title] = v_list[position]
+    return(stats_list)
 
 
 
@@ -42,19 +38,37 @@ if __name__ == '__main__':
     with open('webrtc_internals_dump_inbound_only.txt') as raw_stats:
         all_data = json.load(raw_stats)
 
-    stats_dict = {}
+    # stats_list = list()
 
-    inbound_audio_packets_received_per_second_stats = get_inbound_audio_packets_received_per_second(all_data['PeerConnections']['48752-1']['stats'])[0]
-    inbound_audio_bytes_received_per_second_stats = get_inbound_audio_bytes_received_per_second(all_data['PeerConnections']['48752-1']['stats'])[0]
+    title1, inbound_audio_packets_received_per_second_stats = get_inbound_audio_packets_received_per_second(all_data['PeerConnections']['48752-1']['stats'])
+    title2, inbound_audio_bytes_received_per_second_stats = get_inbound_audio_bytes_received_per_second(all_data['PeerConnections']['48752-1']['stats'])
+    # _ ,inbound_audio_packets_lost = get_inbound_audio_packets_lost(all_data['PeerConnections']['48752-1']['stats'])[0]
+    # _ ,inbound_audio_jitter = get_inbound_audio_jitter(all_data['PeerConnections']['48752-1']['stats'])[0]
+    # _ ,inbound_audio_jitter_buffer_delay = get_inbound_audio_jitter_buffer_delay(all_data['PeerConnections']['48752-1']['stats'])[0]
+    # _ ,inbound_audio_total_samples_received = get_inbound_audio_total_samples_received(all_data['PeerConnections']['48752-1']['stats'])[0]
+    # _ ,inbound_audio_level = get_inbound_audio_level(all_data['PeerConnections']['48752-1']['stats'])[0]
+    # _ ,inbound_audio_energy = get_inbound_audio_energy(all_data['PeerConnections']['48752-1']['stats'])[0]
+    # _ ,inbound_audio_round_trip_time = get_inbound_audio_round_trip_time(all_data['PeerConnections']['48752-1']['stats'])[0]
 
-    empty_timestamp_list = get_timestamp_list(inbound_audio_packets_received_per_second_stats)
+    stats_list = get_timestamp_list(inbound_audio_packets_received_per_second_stats)
 
 
-    list1 = parse_numeric_stats(inbound_audio_packets_received_per_second_stats, 'inbound_audio_packets_received_per_second_stats', empty_timestamp_list)
-    list2 = parse_numeric_stats(inbound_audio_bytes_received_per_second_stats, 'inbound_audio_bytes_received_per_second_stats', list1)
+    list1 = add_stats_to_list(stats_list, inbound_audio_packets_received_per_second_stats, title1)
+    list2 = add_stats_to_list(stats_list, inbound_audio_bytes_received_per_second_stats, title2)
 
     pprint(list2)
 
+inbound_audio_functions = [get_inbound_audio_packets_received_per_second, get_inbound_audio_bytes_received_per_second,
+                           get_inbound_audio_packets_lost, get_inbound_audio_jitter, get_inbound_audio_jitter_buffer_delay,
+                           get_inbound_audio_total_samples_received, get_inbound_audio_level, get_inbound_audio_energy,
+                           get_inbound_audio_round_trip_time]
+
+inbound_video_functions = [get_inbound_video_packets_received_per_second, get_inbound_video_bytes_received_per_second,
+                           get_inbound_video_packets_lost, get_inbound_video_frames_received, get_inbound_video_frames_decoded,
+                           get_inbound_video_frames_decoded_per_second, get_inbound_video_frames_dropped, get_inbound_video_jitter,
+                           get_inbound_video_round_trip_time]
+outbound_audio_functions = [get_outbound_audio_packets_sent_per_second, get_outbound_audio_bytes_sent_per_second]
+outbound_video_functions = []
 
 
 
--- parse_functions.py
+++ parse_functions.py
@@ -1,250 +1,279 @@
 """Inbound Audio"""
 
 def get_inbound_audio_packets_received_per_second(stats_data):
+    title = 'inbound_audio_packets_received_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPAudioStream') and key.endswith('[packetsReceived/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data[0]
 
 
 def get_inbound_audio_bytes_received_per_second(stats_data):
+    title = 'inbound_audio_bytes_received_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPAudioStream') and key.endswith('[bytesReceived_in_bits/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data[0]
 
 
 def get_inbound_audio_packets_lost(stats_data):
+    title = 'inbound_audio_packets_lost'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPAudioStream') and key.endswith('packetsLost'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_audio_jitter(stats_data):
+    title = 'inbound_audio_jitter'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPAudioStream') and key.endswith('jitter'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_audio_jitter_buffer_delay(stats_data):
+    title = 'inbound_audio_jitter_buffer_delay'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPAudioStream') and key.endswith('jitterBufferDelay'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_audio_total_samples_received(stats_data):
+    title = 'inbound_audio_total_samples_received'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPAudioStream') and key.endswith('totalSamplesReceived'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_audio_level(stats_data):
+    title = 'inbound_audio_level'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPAudioStream') and key.endswith('audioLevel'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_audio_energy(stats_data):
+    title = 'inbound_audio_energy'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPAudioStream') and key.endswith('totalAudioEnergy'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_audio_round_trip_time(stats_data):
+    title = 'inbound_audio_round_trip_time'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPAudioStream') and key.endswith('roundTripTime'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 """Inbound Video"""
 
 def get_inbound_video_packets_received_per_second(stats_data):
+    title = 'inbound_video_packets_received_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPVideoStream') and key.endswith('[packetsReceived/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_video_bytes_received_per_second(stats_data):
+    title = 'inbound_video_bytes_received_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPVideoStream') and key.endswith('[bytesReceived_in_bits/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_video_packets_lost(stats_data):
+    title = 'inbound_video_packets_lost'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPVideoStream') and key.endswith('packetsLost'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_video_frames_received(stats_data):
+    title = 'inbound_video_frames_received'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPVideoStream') and key.endswith('framesReceived'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_video_frames_decoded(stats_data):
+    title = 'inbound_video_frames_decoded'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPVideoStream') and key.endswith('framesDecoded'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_video_frames_decoded_per_second(stats_data):
+    title = 'inbound_video_frames_decoded_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPVideoStream') and key.endswith('[framesDecoded/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_video_frames_dropped(stats_data):
+    title = 'inbound_video_frames_dropped'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPVideoStream') and key.endswith('framesDropped'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_video_jitter(stats_data):
+    title = 'inbound_video_jitter'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPVideoStream') and key.endswith('jitter'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_inbound_video_round_trip_time(stats_data):
+    title = 'inbound_video_round_trip_time'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCInboundRTPVideoStream') and key.endswith('roundTripTime'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 """Outbound Audio"""
 
 def get_outbound_audio_packets_sent_per_second(stats_data):
+    title = 'outbound_audio_packets_sent_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPAudioStream') and key.endswith('[packetsSent/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_audio_bytes_sent_per_second(stats_data):
+    title = 'outbound_audio_bytes_sent_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPAudioStream') and key.endswith('[bytesSent_in_bits/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 """Outbound Video"""
 
 def get_outbound_video_packets_sent_per_second(stats_data):
+    title = 'outbound_video_packets_sent_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('[packetsSent/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_video_bytes_sent_per_second(stats_data):
+    title = 'outbound_video_bytes_sent_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('[bytesSent_in_bits/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_video_frames_encoded(stats_data):
+    title = 'outbound_video_frames_encoded'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('framesEncoded'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_video_frames_encoded_per_second(stats_data):
+    title = 'outbound_video_frames_encoded_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('[framesEncoded/s]'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_video_frames_sent(stats_data):
+    title = 'outbound_video_frames_sent'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('framesSent'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_video_quality_limitation_reason(stats_data):
+    title = 'outbound_video_quality_limitation_reason'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('QualityLimitationReason'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_video_quality_limitation_resolution_change(stats_data):
+    title = 'outbound_video_quality_limitation_resolution_change'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('QualityLimitationResolutionChange'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_video_frame_width(stats_data):
+    title = 'outbound_video_frame_width'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('frameWidth'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_video_frame_height(stats_data):
+    title = 'outbound_video_frame_height'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('frameHeight'):
            return_data.append({key: value})
-    return return_data
+    return title, return_data
 
 
 def get_outbound_video_frames_per_second(stats_data):
+    title = 'outbound_video_frames_per_second'
     return_data = []
     for key, value in stats_data.items():
         if key.startswith('RTCOutboundRTPVideoStream') and key.endswith('framesPerSecond'):
            return_data.append({key: value})
-    return return_data
-
+    return title, return_data
 
 
 
